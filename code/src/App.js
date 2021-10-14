73849dbfb3fd7f7d7e5f0cafcb035e4239b6136b
Jenny Quach <quach.jen@gmail.com>
2021-10-14 13:21:44 +0200

    finished Album.js and updated App.js

--- code/src/App.js
+++ code/src/App.js
@@ -5,17 +5,29 @@ import Header from "components/Header";
 
 // import css from "index.css";
 
+
+const albumArray = data.albums.items
+
 export const App = () => { 
 	return (
     <>
       <Header/>
-      {data.albums.items.map((album) => (
-      <Album
-        key={album.id}
-        title={album.name}
-        artist={album.artists}
-        />
-      ))}
-    </>  
+      <div className="container">
+      {albumArray.map((album) => {
+        return (
+          <Album 
+            key={album.name} 
+            title={album.name} 
+            cover={album.images[0].url} 
+            albumurl={album.external_urls.spotify} 
+            artist={album.artists.map((item) => 
+            <a className="link" key={item.name} href={item.external_urls.spotify}><h3 className="artist-name">{item.name}</h3></a>
+           )
+       }
+          />
+        )
+      })}
+      </div>
+    </>
   )
 }
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,18 +1,41 @@
 import React from "react";
-import Artist from "components/Artist";
+// import Artist from "components/Artist";
+
+// const Album = (props) => {
+// console.log(props.title)
+// 	return (
+// 		<div className="artistcard-container">
+// 		<div className="artistcard">
+// 			<div className="albumcover">
+// 				<a href={props.albumContent.external_urls.spotify}>
+// 				<img src={props.albumContent.images[1].url} className="albumimg" alt="albumcover" /></a>
+
+
+// 			<h2>title {album.title}</h2>
+// 			</div>
+// 		</div>	
+// 		</div>		
+// 	)		
+// };
+
 
 const Album = (album) => {
-console.log(album.title)
-
 	return (
-		<div className="cards">
-    	{/* <img src={album.img} alt="img-info"/> */}
-			<h2>title {album.title}</h2>
-			<Artist artists/>
+			<div className="card"> 
+				<div className="cover">
+				<a href={album.albumurl}>         
+					<img className="image" src={album.cover} alt="album cover"/></a>
+					
+			</div>
+					<h2 className="albums-name">{album.name}</h2>
+					<div>
+					<a className="link" href={album.externalurls}>
+					<h3 className="artist-name">{album.artist}</h3></a>           
+			</div>
+			</div>
 
-			{/* <h3 className="artist-name"> <Artist artists = {Artist.name} /></h3> */}
-		</div>		
-	)		
-};
+	)
+}
+
  
 export default Album
